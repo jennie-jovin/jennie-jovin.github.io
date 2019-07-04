b1f3a1cd0e71a2c013fa0e3b458031daf5ddaac1
alcastaneda <adriana@Adrianas-MacBook-Pro.local>
2019-07-04 11:15:54 -0500

    add traditions

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
   const properties = [
     "url('pics/pic1.jpeg') center center no-repeat",
-    "url('pics/pic2.jpeg') 15% top no-repeat",
+    "url('pics/pic2.jpeg') 30%  top no-repeat",
     "url('pics/pic3.jpeg') 25% top no-repeat",
     "url('pics/pic4.jpeg') 25% no-repeat"
   ];
 
   const sizes = [
     "150% auto",
-    "1000px auto",
-    "1000px auto",
-    "1200px auto"
+    "cover",
+    "cover",
+    "cover"
   ]
 
   let i = 0;
