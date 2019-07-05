8710190fd56d4fd9db9c0fd5f46be6cb9ec93c6f
alcastaneda <adriana@Adrianas-MacBook-Pro.local>
2019-07-05 15:24:39 -0400

    add pics

--- js/index.js
+++ js/index.js
@@ -1,13 +1,26 @@
 $(document).ready(function(){
   const properties = [
-    "url('pics/pic1.jpeg') center center no-repeat",
-    "url('pics/pic2.jpeg') 30%  top no-repeat",
-    "url('pics/pic3.jpeg') 25% top no-repeat",
-    "url('pics/pic4.jpeg') 25% no-repeat"
+    "url('pics/pic1.jpg') center center no-repeat",
+    "url('pics/pic2.jpg') top 30% center no-repeat",
+    "url('pics/pic3.jpg') 25% top no-repeat",
+    "url('pics/pic4.jpg') 25% no-repeat",
+    "url('pics/pic5.jpg') top center no-repeat",
+    "url('pics/pic6.jpg') top 25% center no-repeat",
+    "url('pics/pic7.jpg') top 35% center no-repeat",
+    "url('pics/pic8.jpg') top 15% center no-repeat",
+    "url('pics/pic9.jpg') top 35% center no-repeat",
+    "url('pics/pic10.jpg') top 25% center no-repeat"
   ];
 
   const sizes = [
     "150% auto",
+    "cover",
+    "cover",
+    "cover",
+    "cover",
+    "cover",
+    "cover",
+    "cover",
     "cover",
     "cover",
     "cover"
